966f6cea612796fbb28bfdad748a98245655c0bb
Tianguoliang <liangtyson@163.com>
2017-05-31 18:43:45 +0800

    第二次尝试提交

---
+++ config.py
@@ -0,0 +1,12 @@
+MONGO_URL = 'localhost'
+# MONGO_DB = 'campany_informaiton'
+# MONGO_DB = 'informaiton'
+# MONGO_DB = 'informaiton2'
+MONGO_DB = 'informaiton3'
+MONGO_TABLE = 'campany_informaiton'
+GROUP_START=0
+GROUP_END=50
+start = 1
+end = 200000
+keywordlist=['上海市浦东新区','上海市闵行','上海市宝山','上海市嘉定','上海市宝山','上海嘉定区','上海闵行区','上海松江','上海青浦','上海奉贤','上海金山']
+#keyword ='上海市闵行区'
