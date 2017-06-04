90d5086de3950320f7b7fb10fcffb4cad476f5cb
Tianguoliang <liangtyson@163.com>
2017-06-05 00:38:03 +0800

    修改了公司公告无法正确显示；增加了代理，但是未能正常工作

--- company.py
+++ company.py
@@ -14,53 +14,93 @@ from multiprocessing import Pool
 import random
 client = pymongo.MongoClient(MONGO_URL, connect=False)
 db = client[MONGO_DB]
-
-def search_company_information(keyword,p):
-    session = requests.session()
+proxy=None
+def get_proxy():
+    s=[]
     try:
-        a={'http':'60.19.233.141:46928'},{'http':'113.121.186.147:29287'},{'http':'175.147.116.27:43751'}#需要自己添加代理，否则IP容易被封，跳出验证码
-        session.proxies=random.choice(a)
+        abc=requests.get('http://www.xdaili.cn/ipagent//freeip/getFreeIps?page=1&rows=10')
+        if abc.status_code==200:
+            lines=(abc.json().get('rows'))
+            for line in lines:
+                ip=line.get('ip')
+                port=line.get('port')
+                response=str(ip)+':'+str(port)
+                s.append(response)
+            return s
+        return None
+    except ConnectionError:
+        return None
+
+def search_company_information(keyword,p,count=1):
+    print(keyword)
+    print(p)
+    max_count=10
+    global proxy
+    if count>=max_count:
+        print('请求次数过多')
+        return None
+    session = requests.session()
+    session.proxies=proxy
+    try:
+    # try:
+    #     a={'http':'60.19.233.141:46928'},{'http':'113.121.186.147:29287'},{'http':'175.147.116.27:43751'}#需要自己添加代理，否则IP容易被封，跳出验证码
+    #     session.proxies=random.choice(a)
+    #     b="111.20.214.25",'200.29.191.151'
+    #     session.cookies.set("tnet", random.choice(b))
+    # except:
+    #     print('重新加载代理')
+    #     return search_company_information(keyword,p)
         b="111.20.214.25",'200.29.191.151'
         session.cookies.set("tnet", random.choice(b))
-    except:
-        print('重新加载代理')
-        return search_company_information(keyword,p)
-    start_url = 'http://www.tianyancha.com/search/or'
-    keyword8=parse.quote(keyword)   #把索引关键字转换为Unicode码
-    public_headerses={"User-Agent":'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'},\
-                 {"User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36"},
-    {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0"}
-    public_headers =random.choice(public_headerses)
-    api_headers = public_headers.copy()
-    index_url = start_url + str(start) + str(end)+'/p{}?key='.format(str(p))+str(keyword8)
-    # print('index_url')
-    # print(index_url)
-    tongji_url = "http://www.tianyancha.com/tongji/"+keyword+str(".json?random=%s") % (int(time.time()) * 1000)
-    api_url = "http://www.tianyancha.com/search/"+keyword+str(".json?&pn=%s&moneyStart=%s&moneyEnd=%s") % (p,start,end)
-    # print('api_url')
-    # print(api_url)
-    api_headers.update({
-        "Tyc-From": "normal",
-        "Accept": "application/json, text/plain, */*",
-        "Referer": index_url,
-        "Accept-Encoding":"gzip, deflate, sdch"
-    })
+        start_url = 'http://www.tianyancha.com/search/or'
+        keyword8=parse.quote(keyword)   #把索引关键字转换为Unicode码
+        public_headerses={"User-Agent":'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'},\
+                     {"User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36"},
+        {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0"}
+        public_headers =random.choice(public_headerses)
+        api_headers = public_headers.copy()
+        index_url = start_url + str(start) + str(end)+'/p{}?key='.format(str(p))+str(keyword8)
+        # print('index_url')
+        # print(index_url)
+        tongji_url = "http://www.tianyancha.com/tongji/"+keyword+str(".json?random=%s") % (int(time.time()) * 1000)
+        api_url = "http://www.tianyancha.com/search/"+keyword+str(".json?&pn=%s&moneyStart=%s&moneyEnd=%s") % (p,start,end)
+        # print('api_url')
+        # print(api_url)
+        api_headers.update({
+            "Tyc-From": "normal",
+            "Accept": "application/json, text/plain, */*",
+            "Referer": index_url,
+            "Accept-Encoding":"gzip, deflate, sdch"
+        })
     # print('api-heders')
     # print (api_headers)
-    index_page = session.request("GET", index_url, headers = public_headers)
+        print('等待请求index_page')
+        print(index_url)
+        index_page = session.request("GET", index_url, headers = public_headers)
+        print(index_page.status_code)
+        soup = BeautifulSoup(index_page.text,'lxml')
+        a=(soup.select('script'))
 
-    soup = BeautifulSoup(index_page.text,'lxml')
-    a=(soup.select('script'))
-
-    s=re.compile('src="(.*?)">',re.S)
-    js_url=re.findall(s,str(a))[0]
-    # print('js_url')
-    # print (js_url)
-    js_page = session.request("GET", js_url, headers = public_headers)
-
+        s=re.compile('src="(.*?)">',re.S)
+        js_url=re.findall(s,str(a))[0]
+        # print('js_url')
+        # print (js_url)
+        print('等待请求js_page')
+        print(js_url)
+        js_page = session.request("GET", js_url, headers = public_headers)
+        print(js_page.status_code)
+    except:
+        print('使用代理。。。。。。。。。。。。。。。。。。。。。。。。。。。。')
+        if count==1:
+            s=get_proxy()
+        for line in s:
+            proxy={'http':str(line)}
+            print(proxy)
+            count+=1
+            return search_company_information(keyword,p,count)
     try:
         sgattrs = json.loads(re.findall(r"n\._sgArr=(.+?);", str(js_page.content))[0])
-    except json.JSONDecodeError:
+    except:
         print('求sgattrs出错')
         return None
     tongji_page = session.request("GET", tongji_url, headers = api_headers)
@@ -286,7 +326,7 @@ def get_getlawsuit(company_name):
         return None
 #获取法院公告
 def get_court(company_name):
-    url='http://www.tianyancha.com/v2/getlawsuit/'+company_name+'.json?'
+    url='http://www.tianyancha.com/v2/court/'+company_name+'.json?'
     response=requests.get(url)
     try:
         if response.status_code==200 and  'courtAnnouncements' in response.json():
@@ -309,7 +349,7 @@ def get_zhixing(id):
                 return zhixing
         return None
     except RequestException:
-        print('获取法院公告失败')
+        print('获取公司司法中的被执行人失败')
         return None
 
 #获取公司经营异常
@@ -460,95 +500,93 @@ def save_to_mongo(all_information):
 def main(p):
     # p_end=50
     # for p in range(1,p_end):   #P为页数
-    # keywordlist=['北京百度网讯科技有限公司']
-    keywordlist=['上海市浦东','上海市闵行','上海市宝山','上海市嘉定','上海市宝山','上海嘉定区','上海闵行区','上海松江','上海青浦','上海奉贤','上海金山']
+    # keywordlist=['上海市闵行']
+    # keywordlist=['上海市浦东','上海市闵行','上海市宝山','上海市嘉定','上海市宝山','上海嘉定区','上海闵行区','上海松江','上海青浦','上海奉贤','上海金山']
     for keyword in keywordlist:
-        print(keyword)
-        print(p)
-
         company_informations=search_company_information(keyword,p)
-        for company_information in company_informations:
-            try:
-                #获取重要的公司名称和id
-                id=(company_information).get('id')
-                company_name=company_information.get('name').replace('<em>','').replace('</em>','')
-                print(id)
-                print(company_name)
+        if company_informations is not None:
+            for company_information in company_informations:
+                try:
+                    #获取重要的公司名称和id
+                    id=(company_information).get('id')
+                    company_name=company_information.get('name').replace('<em>','').replace('</em>','')
+                    print(id)
+                    print(company_name)
 
-                #由id获取公司信息
-                print('获取公司信息中')
-                annureport_count=get_annureport_count(id)
-                nianbaos=get_nianbao(annureport_count)
-                staff_informaton=get_staff_informaton(id)
-                holder_informaton=get_holder_informaton(id)
-                inverst_information=get_inverst_informaton(id)
-                changeinfo_informaton=get_changeinfo_informaton(id)
-                branch_informaton=get_branch_informaton(id)
+                    #由id获取公司信息
+                    print('获取公司信息中')
+                    annureport_count=get_annureport_count(id)
+                    nianbaos=get_nianbao(annureport_count)
+                    staff_informaton=get_staff_informaton(id)
+                    holder_informaton=get_holder_informaton(id)
+                    inverst_information=get_inverst_informaton(id)
+                    changeinfo_informaton=get_changeinfo_informaton(id)
+                    branch_informaton=get_branch_informaton(id)
 
 
-                #由name获取公司发展信息
-                print('获取公司发展信息中')
-                findHistoryRongzi=get_findHistoryRongzi(company_name)
-                findTeamMember=get_findTeamMember(company_name)
-                findProduct=get_findProduct(company_name)
-                findTzanli=get_findTzanli(company_name)
-                findJingpin=get_findJingpin(company_name)
+                    #由name获取公司发展信息
+                    print('获取公司发展信息中')
+                    findHistoryRongzi=get_findHistoryRongzi(company_name)
+                    findTeamMember=get_findTeamMember(company_name)
+                    findProduct=get_findProduct(company_name)
+                    findTzanli=get_findTzanli(company_name)
+                    findJingpin=get_findJingpin(company_name)
 
-                #由name获取公司司法风险信息
-                print('获取公司司法风险信息中')
-                getlawsuit=get_getlawsuit(company_name)
-                court=get_court(company_name)
-                zhixing=get_zhixing(id)
+                    #由name获取公司司法风险信息
+                    print('获取公司司法风险信息中')
+                    getlawsuit=get_getlawsuit(company_name)
+                    court=get_court(company_name)
+                    zhixing=get_zhixing(id)
 
-                #由name获取公司经营风险信息
-                print('获取公司经营风险信息中')
-                abnormal=get_abnormal(id)
-                punishment=get_punishment(company_name)
-                companyEquity=get_companyEquity(company_name)
+                    #由name获取公司经营风险信息
+                    print('获取公司经营风险信息中')
+                    abnormal=get_abnormal(id)
+                    punishment=get_punishment(company_name)
+                    companyEquity=get_companyEquity(company_name)
 
-                #由id获取公司经营状况
-                print('获取公司经营状况信息中')
-                bid=get_bid(id)
-                getEmploymentList=get_getEmploymentList(company_name)
-                appbkinfo=get_appbkinfo(id)
+                    #由id获取公司经营状况
+                    print('获取公司经营状况信息中')
+                    bid=get_bid(id)
+                    getEmploymentList=get_getEmploymentList(company_name)
+                    appbkinfo=get_appbkinfo(id)
 
-                #获取公司商标信息
-                print('获取公司商标信息中')
-                getTmList=get_getTmList(id)
-                patent=get_patent(id)
+                    #获取公司商标信息
+                    print('获取公司商标信息中')
+                    getTmList=get_getTmList(id)
+                    patent=get_patent(id)
 
-                if company_information is not None:
-                    all_information={'公司名称':company_name,
-                                     '公司基本信息':company_information,
-                                     '公司主要人员':staff_informaton,
-                                     '公司股东信息':holder_informaton,
-                                     '公司对外投资信息':inverst_information,
-                                     '公司变更记录':changeinfo_informaton,
-                                     '公司年报':nianbaos,
-                                     '公司分支机构':branch_informaton,
-                                     '公司融资历史':findHistoryRongzi,
-                                     '公司核心团队':findTeamMember,
-                                     '公司企业业务':findProduct,
-                                     '公司投资事件':findTzanli,
-                                     '竞品信息':findJingpin,
-                                     '法律诉讼信息':getlawsuit,
-                                     '法院公告':court,
-                                     '公司法律被执行人':zhixing,
-                                     '公司经营异常':abnormal,
-                                     '公司行政处罚':punishment,
-                                     '公司股权出质':companyEquity,
-                                     '公司招投标':bid,
-                                     '公司招聘信息':getEmploymentList,
-                                     '公司产品信息':appbkinfo,
-                                     '公司商标信息':getTmList,
-                                     '公司专利信息':patent
-                                     }
-                    save_to_mongo(all_information)
-            except:
-                print('获取此公司信息失败')
+                    if company_information is not None:
+                        all_information={'公司名称':company_name,
+                                         '公司基本信息':company_information,
+                                         '公司主要人员':staff_informaton,
+                                         '公司股东信息':holder_informaton,
+                                         '公司对外投资信息':inverst_information,
+                                         '公司变更记录':changeinfo_informaton,
+                                         '公司年报':nianbaos,
+                                         '公司分支机构':branch_informaton,
+                                         '公司融资历史':findHistoryRongzi,
+                                         '公司核心团队':findTeamMember,
+                                         '公司企业业务':findProduct,
+                                         '公司投资事件':findTzanli,
+                                         '竞品信息':findJingpin,
+                                         '法律诉讼信息':getlawsuit,
+                                         '法院公告':court,
+                                         '公司法律被执行人':zhixing,
+                                         '公司经营异常':abnormal,
+                                         '公司行政处罚':punishment,
+                                         '公司股权出质':companyEquity,
+                                         '公司招投标':bid,
+                                         '公司招聘信息':getEmploymentList,
+                                         '公司产品信息':appbkinfo,
+                                         '公司商标信息':getTmList,
+                                         '公司专利信息':patent
+                                         }
+                        save_to_mongo(all_information)
+                except:
+                    print('获取此公司信息失败')
 
 if __name__ == "__main__":
-    # main(6)
+    # main(16)
     p = ([x for x in range(GROUP_START, GROUP_END + 1)])
     pool = Pool(8)
     pool.map(main, p)
